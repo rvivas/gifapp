0df5909a5a5f70dc1d19fa11ad77aab39511cc41
rvivas <rubenarturo.vivas@gmail.com>
2020-07-04 15:32:49 -0400

    add docs

---
+++ docs/precache-manifest.19620394ecee3c3a99a8c1a88379d94b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "957d333aa68eef7f18674bc4f0766e4d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bad67b8cacfc6643a691",
+    "url": "/static/css/main.97a8a0b2.chunk.css"
+  },
+  {
+    "revision": "6d42d2aa551302d4bb3d",
+    "url": "/static/js/2.9d74f133.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9d74f133.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bad67b8cacfc6643a691",
+    "url": "/static/js/main.000a7eee.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
